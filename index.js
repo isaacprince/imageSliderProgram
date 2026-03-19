0e54054b255dfa7e92dc2f874ec46778852525fd
isaacprince <isaacprince002@gmail.com>
2026-03-19 11:56:17 -0400

    feat:create functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+
+
+function initializeSlider(){
+     slides[]
+}
+
+function showSlide(index){
+
+}
+
+function prevSlide(){
+
+}
+
+function nextSlide(){
+
+}
